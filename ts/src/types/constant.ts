26b9cb668bcaf5a186fd35cece85f48b414f7240
Nartallax <nartallax@gmail.com>
2023-05-04 07:02:44 +0400

    1.0.9: fix stupid bug about object as constant

--- ts/src/types/constant.ts
+++ ts/src/types/constant.ts
@@ -5,7 +5,6 @@ const noOpt = {}
 const knownConstantTypes = new Map<RC.Constantable, RC.Constant>()
 
 // TODO: const T here
-export function rcConstant<T extends RC.Constantable>(base: RC.ConstantDefinition<T>): RC.Constant<T>
 export function rcConstant<T extends RC.Constantable>(base: RC.ConstantDefinition<T>, valueType: T): RC.Constant<T>
 export function rcConstant<T extends RC.Constantable>(valueType: T): RC.Constant<T>
 export function rcConstant<T extends RC.Constantable>(a: RC.ConstantDefinition<T> | T, b?: T): RC.Constant<T> {
