6704a23ddc2edb44c3304daa79f141fdc93ddea4
Nartallax <nartallax@gmail.com>
2023-05-04 07:06:12 +0400

    1.0.10: fix constant argument resolving... again

--- ts/src/types/constant.ts
+++ ts/src/types/constant.ts
@@ -1,5 +1,4 @@
 import type {RC} from "src/ribcage"
-import {resolveTwoArguments} from "src/types/base"
 
 const noOpt = {}
 const knownConstantTypes = new Map<RC.Constantable, RC.Constant>()
@@ -7,8 +6,17 @@ const knownConstantTypes = new Map<RC.Constantable, RC.Constant>()
 // TODO: const T here
 export function rcConstant<T extends RC.Constantable>(base: RC.ConstantDefinition<T>, valueType: T): RC.Constant<T>
 export function rcConstant<T extends RC.Constantable>(valueType: T): RC.Constant<T>
-export function rcConstant<T extends RC.Constantable>(a: RC.ConstantDefinition<T> | T, b?: T): RC.Constant<T> {
-	const [def, _value] = resolveTwoArguments<RC.ConstantDefinition<T>, T>(a, b, noOpt)
+export function rcConstant<T extends RC.Constantable>(...args: (RC.ConstantDefinition<T> | T)[]): RC.Constant<T> {
+	// cannot use `resolveTwoArguments` here because value can be undefined and that's legit value
+	let def: RC.ConstantDefinition<T>
+	let _value: T
+	if(args.length === 2){
+		def = args[0] as RC.ConstantDefinition<T>
+		_value = args[1] as T
+	} else {
+		def = noOpt
+		_value = args[0] as T
+	}
 	const value = "value" in def ? def.value! : _value
 
 	if(def === noOpt){
