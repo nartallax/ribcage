68aadcaf9b88129748885be04d1c1e8ef8fa3a09
Nartallax <nartallax@gmail.com>
2023-05-06 21:35:24 +0400

    Remove unused file

--- ts/src/types/base.ts
+++
@@ -1,7 +0,0 @@
-export function resolveTwoArguments<A, B>(a: A | B, b: B | undefined, defaultA: A): [A, B] {
-	if(b === undefined){
-		return [defaultA, a as B]
-	} else {
-		return [a as A, b]
-	}
-}
